40643726710edb2d7e295238f988010e07d0b912
BrickPi <49528459+BrickPi@users.noreply.github.com>
2022-12-15 03:46:19 +0000

    Update shader.h

--- inc/subterra/gfx/shader.h
+++ inc/subterra/gfx/shader.h
@@ -5,8 +5,8 @@
 #include "cglm/cglm.h"
 #include "subterra/util/logger.h"
 void shader_load();
-inline void shader_use_flat();
-inline void shader_use_instanced();
+void shader_use_flat();
+void shader_use_instanced();
 void shader_clean();
 void shader_uniforms(mat4* proj, mat4* view, mat4* model);
 void set_instance_uniform(int count, vec3* arr);
